8a27a7912d999788eb3c568434b650ea8ac7f451
alex-karev <karev.as99@gmail.com>
2025-03-18 17:34:36 +0800

    Add multilanguage support

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
 import sys
-from PySide6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QTabWidget,
-    QWidget,
-    QVBoxLayout,
-    QLabel
-)
+from PySide6.QtWidgets import QApplication, QMainWindow
+import locale
 from core import load_config
 from editor import Editor
 from translations import get_lang
+
 
 class TabbedApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.prefs = load_config()
-        self.lang = get_lang(self.prefs.app_language)
-       
+        current_locale, encoding = locale.getdefaultlocale()
+        current_locale = "en" if not "ru" in current_locale.lower() else "ru"
+        self.lang = get_lang(current_locale)
+
         # Set up the main window
         self.setWindowTitle("Simple Tabbed Layout App")
-        #self.setGeometry(100, 100, 300, 200)
-        
+        # self.setGeometry(100, 100, 300, 200)
+
         # Create a QTabWidget
         self.editor = Editor(self.prefs, self.lang)
         self.setCentralWidget(self.editor)
-   
+
+
 # Start app
 if __name__ == "__main__":
     app = QApplication(sys.argv)
